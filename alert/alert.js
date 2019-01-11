64da16a73f2fb4f2ec7f84faece4d07ea534c6e6
AbhijeetPande <abhijeet.cbse@gmail.com>
2019-01-11 15:09:36 -0500

    better version, involving ignoreCase

--- alert/alert.js
+++ alert/alert.js
@@ -1,11 +1,20 @@
-let answer;
+let primaryConfirmation = prompt("Who's There? ", "");
+let secondaryConfirmation;
+//checking all the possibilities for the primary confirmation
 
-answer = prompt ("What is the "+ "\"official\"" + " name of Javascript?")
-
-if(answer == "ECMAScript"){
-    alert("Right!");
+if ( primaryConfirmation == null || primaryConfirmation == ""  || primaryConfirmation.toLowerCase() == "cancel"){
+    alert("Cancelled.");
+}else if(primaryConfirmation.toLowerCase() == "admin"){
+    secondaryConfirmation = prompt("Password Please? ", "Enter Password here");
+    //checking all the possibilities for the secondary confirmation
+    if(secondaryConfirmation == "TheMaster"){
+        alert("Welcome!");
+    }else if(secondaryConfirmation == null || secondaryConfirmation.toLowerCase() == "cancel" || secondaryConfirmation == ""){
+        alert("Cancelled.");
+    }else{
+        alert("Wrong Password.");
+    }
+}else{
+    alert("I don't know you. Go away >:(");
 }
 
-else{
-    alert("You didn't know? \"ECMAScript\"!");
-}
