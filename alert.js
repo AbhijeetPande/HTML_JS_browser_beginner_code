74fdfb4bd895d2f185195cd0b0d01a57015f6440
AbhijeetPande <abhijeet.cbse@gmail.com>
2019-01-11 10:31:36 -0500

    first commit

---
+++ alert.js
@@ -0,0 +1,11 @@
+let answer;
+
+answer = prompt ("What is the "+ "\"official\"" + " name of Javascript?")
+
+if(answer == "ECMAScript"){
+    alert("Right!");
+}
+
+else{
+    alert("You didn't know? \"ECMAScript\"!");
+}
